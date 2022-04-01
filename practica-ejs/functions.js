ddc7ec86d8c5dda88e6589adc335c04f65a09373
sebastian frausto <100221988+sebaszap123-dev@users.noreply.github.com>
2022-04-01 09:04:28 -0600

    init de la practica EJS

---
+++ practica-ejs/functions.js
@@ -0,0 +1,17 @@
+function isPar(n){
+    if(n != Number) return;
+    if(2 % n == 0){
+        return isPar;
+    }
+}
+
+function isImpar(n){
+    
+}
+
+isPar => {
+    isPar: 'Es par'
+    isPar: 'Es par'
+    isPar: 'Es par'
+    isPar: 'Es par'
+}
