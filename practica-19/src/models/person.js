b3aeeea6a19a49ecd645f55a44befa3a83179472
sebaszap123-dev <freeway.dev.sf@gmail.com>
2022-05-18 08:07:35 -0500

    practica-19

---
+++ practica-19/src/models/person.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const PersonSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Person", PersonSchema);
