c059745ccd28b5aac8376ed3fd85df33adb62382
sebaszap123-dev <freeway.dev.sf@gmail.com>
2022-05-20 07:03:36 -0500

    added comments and finish

--- practica-20/src/routes/router.js
+++ practica-20/src/routes/router.js
@@ -14,9 +14,16 @@ router.get("/persons", function (req, res, next) {
 });
 
 router.post("/persons", async (req, res, next) => {
+  // Aquí hacemos la petición del body parseado
   const { fname, lname } = req.body;
   const newPerson = new Person({ firstName: fname, lastName: lname });
   console.log(newPerson);
+  // Desde la practica 20 lo habia implementado pero en esta ocasión hablare más del como y porque ya lo habia implementado
+  /* Esta practica trata de llevar acabo el guardado de datos en la base de datos, el método save esta proporcionado por mongoose
+  Permitiendonos guardar los modelos de datos que creamos en dicha base de datos a la que previamente estamos conectados
+  En mi caso las conexiones estan hechas mediante variables de entorno que no subiré a github pero mencionarlas que son
+  MONGODB_URI y TESTING
+  */
   await newPerson.save();
   res.send(`First name es: ${fname} Last name es: ${lname}`);
   console.log(req.body);
